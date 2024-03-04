422c44c2d9bfd2e80b18bdb18c2b407124bf445b
Your Name <you@example.com>
2024-03-04 20:35:48 +0000

    added navbar and routes

---
+++ vite-react-portfolio/src/components/pages/projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function Projects() {
+    return (
+      <div>
+        <h1>About Page</h1>
+        <p>
+       TEST TEXT
+        </p>
+      </div>
+    );
+  }
+  export default Projects;
